6c5ee486904a12aa2a780b3f1e2b8de6aec85d20
alec <alec@petros.audio>
2017-12-05 19:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value});
 }
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj[key];
+  var testObj = Object.assign({}, object);
+  delete testObj[key];
   newObj;
 }
