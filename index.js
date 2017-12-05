64297bf504b4f57fa520f52c82a2d8a5b6e33449
alec <alec@petros.audio>
2017-12-05 19:52:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
-function updateObjectWIthKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value});
 }
